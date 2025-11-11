9b9f6e6bcbaeae2969304e9bccf5a64748235932
NauvalAkbar <akbarnauval2809@gmail.com>
2025-11-11 15:47:46 +0700

    Create api_token.js

---
+++ api/api_token.js
@@ -0,0 +1,5 @@
+// app/api/api_token.js
+// GANTI DENGAN TOKEN KAMU SENDIRI!
+
+export const GITHUB_TOKEN = "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"; // GitHub PAT
+export const VERCEL_TOKEN = "JqLngTK6jAdDiptg4DogBGrB"; // Vercel API Token
